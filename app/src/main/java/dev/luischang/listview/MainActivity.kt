b458f15b60a798c6da6fadfcc6b099396fc5ecc8
Luis Chang <luis.chang@outlook.com>
2024-09-07 10:33:34 -0500

    Initial commit

---
+++ app/src/main/java/dev/luischang/listview/MainActivity.kt
@@ -0,0 +1,43 @@
+package dev.luischang.listview
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var arrSO = arrayOf("Android","iOS","Blackberry OS",
+        "Firefox OS", "Windows Phone", "Nokia OS")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val lsvSO: ListView = findViewById(R.id.lsvSO)
+
+        val adaptador: Any ? =
+            ArrayAdapter<Any?>(this,android.R.layout.simple_list_item_1,arrSO)
+
+        lsvSO.adapter = adaptador as ListAdapter?
+
+        lsvSO.setOnItemClickListener {
+                                     parent, view, position, l ->
+
+            Toast.
+                makeText(this,"Posición: $position", Toast.LENGTH_LONG)
+                    .show()
+
+
+        }
+
+    }
+}
